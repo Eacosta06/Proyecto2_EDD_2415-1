9a6ec5778b53021f5a4cde8325fa33d500b60171
Eacosta <esteacosta@Estebans-MacBook-Pro.local>
2024-11-06 17:20:45 -0400

    Imports defectuosos de librerías GraphStream y Gson

---
+++ Proyecto2EDD/src/proyecto2edd/Inicializar.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto2edd;
+
+import Estructuras.*;
+//import org.graphstream.graph.*;
+//import com.google.gson.*;
+
+/**
+ *
+ * @author esteacosta
+ * Esta clase lee el String del archivo Json y lo convierte a
+ * las respectivas estructuras de datos y a Graphstream utilizando
+ * la libreria GSON de Google.
+ */
+public class Inicializar {
+
+    public Inicializar() {
+    }
+    
+    public void Iniciar(String JsonString){
+        
+        //Gson gson = new Gson();
+        
+        //JsonElement json;
+        
+    }
+    
+}
