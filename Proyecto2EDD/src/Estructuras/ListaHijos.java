2991d4e1f87afd22ed91e05c8d6a24ce2f6ee4c9
Eacosta <esteacosta@Estebans-MacBook-Pro.local>
2024-11-06 17:28:38 -0400

    merge con develop

---
+++ Proyecto2EDD/src/Estructuras/ListaHijos.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Estructuras;
+
+/**
+ *
+ * @author carluchocp
+ */
+public class ListaHijos {
+    private NodoHijos pFirst;
+    private String nombre;
+    private int iN;
+
+    public ListaHijos(String nombre) {
+        this.pFirst = null;
+        this.nombre = nombre;
+        this.iN = 0;
+    }
+
+    public NodoHijos getpFirst() {
+        return pFirst;
+    }
+
+    public void setpFirst(NodoHijos pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getiN() {
+        return iN;
+    }
+}
